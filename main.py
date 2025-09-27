52c8940eaaad7f3b06c7ea313cc6446ab49e848b
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-09-27 11:50:21 +0300

    [update] add code lessons

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-def main():
+def sin():
     # Створюємо дані
     x = np.linspace(0, 10, 100) # 100 точок від 0 до 10
     y = np.sin(x) # Значення синуса для кожної точки x
@@ -15,6 +16,30 @@ def main():
     plt.grid(True) # Включаємо сітку
     plt.show()
 
+# sin()
 
-if __name__ == '__main__':
-    main()
+def bar():
+    data = {
+        "Місяць": ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень'],
+        "Продажі": [15000, 180000, 220000, 170000, 250000]
+    }
+
+    df = pd.DataFrame(data)
+
+    # Створюємо стовпчасту діаграму
+    plt.figure(figsize=(12, 6))
+    # Створюємо стовпці
+    bars = plt.bar(df['Місяць'], df['Продажі'], color=['#FF6B6B', '#4ECDC4', '#45B7D1', '#96CEB4', '#FFEAA7'])
+
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2., height+500, f'int{height}', ha = 'center', va='bottom')
+
+    plt.title('Продажі по місяцях', fontsize=16, fontweight="bold")
+    plt.xlabel("Місяць")
+    plt.ylabel('Продаіжі (грн)')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.show()
+
+bar()
